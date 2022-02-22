5bae8fb58f7be8cef5d33ac216f37057d4777deb
Everton Bruno Silva dos Santos <37885125+evertonbrunosds@users.noreply.github.com>
2022-02-22 19:45:33 -0300

    Telas iniciais

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  const ProductsOverviewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(final BuildContext context) {
+    return Container();
+  }
+}
