3baa9069004cee52b00d044d0b51f1fdc50f6b27
Everton Bruno Silva dos Santos <37885125+evertonbrunosds@users.noreply.github.com>
2022-02-23 21:00:51 -0300

    Refatoração da Grade de Produtos

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,33 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/Product.dart';
-import 'package:shop/models/product_list.dart';
+import '../components/product_grid.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   ProductsOverviewScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(final BuildContext context) {
-  final ProductList provider = Provider.of<ProductList>(context);
-  final List<Product> loadedProducts = provider.itens;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
         centerTitle: true,
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (context, index) => ProductItem(product: loadedProducts[index]),
-      ),
+      body: ProductGrid(),
     );
   }
 }
