0a73fccf8b9c230735a9c8abd5497ea9eac9bbdc
Everton Bruno Silva dos Santos <37885125+evertonbrunosds@users.noreply.github.com>
2022-03-01 16:35:52 -0300

    Adição: carregamento de produtos via API

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
 import 'package:shop/models/order.dart';
 import '../utils/constants.dart';
 
@@ -12,6 +13,34 @@ class OrderList with ChangeNotifier {
   List<Order> get itens => [..._itens];
 
   int get itensCount => _itens.length;
+
+  Future<void> loadOrders() async {
+    _itens.clear();
+    final response =
+        await http.get(Uri.parse('${Constants.ORDER_BASE_URL}.json'));
+    if (response.body != 'null') {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      data.forEach((orderId, orderData) {
+        _itens.add(
+          Order(
+            id: orderId,
+            date: DateTime.parse(orderData['date']),
+            total: orderData['total'],
+            products: (orderData['products'] as List<dynamic>)
+                .map((e) => CartItem(
+                      id: e['id'],
+                      productId: e['productId'],
+                      name: e['name'],
+                      quantity: e['quantity'],
+                      price: e['price'],
+                    ))
+                .toList(),
+          ),
+        );
+      });
+      notifyListeners();
+    }
+  }
 
   Future<void> addOrder({required final Cart cart}) async {
     final date = DateTime.now();
@@ -29,7 +58,8 @@ class OrderList with ChangeNotifier {
                 'quantity': cartItem.quantity,
                 'price': cartItem.price,
               },
-            ).toList(),
+            )
+            .toList(),
       }),
     );
     final id = jsonDecode(response.body)['name'];
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,8 +4,32 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order_widget.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   const OrdersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<OrderList>(
+      context,
+      listen: false,
+    ).loadOrders().then((_) {
+      setState(() => isLoading = false);
+    });
+  }
+
+  Future<void> _refreshOrders(final BuildContext context) =>
+      Provider.of<OrderList>(
+        context,
+        listen: false,
+      ).loadOrders();
 
   @override
   Widget build(final BuildContext context) {
@@ -20,9 +44,17 @@ class OrdersScreen extends StatelessWidget {
       drawer: const AppDrawer(),
       body: Container(
         color: colorScheme.primaryContainer,
-        child: ListView.builder(
-          itemCount: orders.itensCount,
-          itemBuilder: (ctx, index) => OrderWidget(order: orders.itens[index]),
+        child: RefreshIndicator(
+          onRefresh: () => _refreshOrders(context),
+          child: isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : ListView.builder(
+                  itemCount: orders.itensCount,
+                  itemBuilder: (ctx, index) =>
+                      OrderWidget(order: orders.itens[index]),
+                ),
         ),
       ),
     );
